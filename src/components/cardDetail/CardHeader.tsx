12327c5f4a47a067b7bb59fd137fbeab82906b73
Hengyu Jiang <893091053@qq.com>
2026-07-13 17:03:01 +0800

    feat: add settlementDtail Page& refactor: refactor repeated code into components

---
+++ src/components/cardDetail/CardHeader.tsx
@@ -0,0 +1,42 @@
+import { Card } from "@/types/card";
+
+type Props = {
+  card: Card;
+  children?: React.ReactNode
+};
+
+export default function CardHeader({ card, children }: Props) {
+  return (
+    <div>
+      <div className="bg-white rounded-2xl shadow-md p-8">
+        <div className="flex items-center gap-5">
+          <img
+            src={
+              card.icon ??
+              `https://www.google.com/s2/favicons?domain=${card.link}&sz=48`
+            }
+            alt={card.name}
+            width={64}
+            height={64}
+            className="rounded-xl"
+          />
+
+          <div className="flex-1">
+            <h1 className="text-3xl font-bold">{card.name}</h1>
+
+            <p className="text-gray-500 mt-1">{card.link}</p>
+          </div>
+        </div>
+
+        <p className="text-gray-600 leading-8 mt-4">
+          {card.description}
+        </p>
+        
+        <div>
+            {children}
+        </div>
+        
+      </div>
+    </div>
+  );
+}
---
+++ src/components/cardDetail/SettlementDetail.tsx
@@ -0,0 +1,26 @@
+import CardHeader from "./CardHeader";
+import { SettlementCard } from "@/types/card";
+import StatusIcon from "@/components/StatusIcon"
+
+type Props = {
+  card: SettlementCard;
+};
+
+export default function SettlementDetail({ card }: Props) {
+  return (
+    <div className="w-full min-h-screen bg-gray-50 flex justify-center py-10">
+      <div className="w-full max-w-3xl px-6">
+
+        <CardHeader card={card}>
+          <StatusIcon card={card}/>
+        </CardHeader>
+
+
+        <div className="mt-8 h-40 rounded-2xl border-2 border-dashed border-gray-300 flex items-center justify-center text-gray-400 bg-white">
+          广告位
+        </div>
+
+      </div>
+    </div>
+  );
+}
